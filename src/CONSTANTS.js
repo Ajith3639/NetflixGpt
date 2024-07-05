968a2596a1b5b45b21cc9d0c7ccd835ed8039ef2
Your Name <your-email@example.com>
2024-07-05 16:25:32 +0530

    Move keys to env file

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -2,8 +2,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MzA0Y2VkZmY2OTFiNDkwYjY3MjA2YzFmMmE4NjdjYiIsInN1YiI6IjY2NzNmNzE4MTdjNjliMzc3NzkzZDVjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h66XY7ta1f6DZaYgAcgOqWoKoflXFruyqa-5c5cH20M",
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
   },
 };
 
